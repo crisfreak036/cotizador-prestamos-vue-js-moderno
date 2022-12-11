53944542e4167a2932c54275ae96a8fb8dae15e3
crisfreak <cristopher.sotog@utem.cl>
2022-12-10 22:07:41 -0300

    [add] input range al componente principal junto a la función encargada de manejar los cambios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
   import Header from './components/Header.vue';
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
 </script>
 
 <template>
@@ -7,5 +10,15 @@
     class="my-20 max-w-lg mx-auto bg-white shadow p-10"
   >
     <Header/>
+
+    <div class="my-5">
+      <input 
+        type="range" 
+        name="" 
+        id="" 
+        className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600 mt-5'
+        @input="handleChange"
+      />
+    </div>
   </div>
 </template>
