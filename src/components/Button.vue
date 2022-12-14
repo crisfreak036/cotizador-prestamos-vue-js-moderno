5420b972334a715445d612698af724bf38daad5c
crisfreak <cristopher.sotog@utem.cl>
2022-12-13 22:17:52 -0300

    [add] componente Button

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup>
+    // const { tipoBoton, contenidoTexto, handle } = defineProps(['tipoBoton', 'contenidoTexto', 'handle'])
+    const props = defineProps({
+        tipoBoton: String, 
+        contenidoTexto: String, 
+        handle: Function
+    })
+</script>
+
+<template>
+    <button
+        :type="tipoBoton"
+        className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+        @click="handle"
+    >
+    {{contenidoTexto}}
+    </button>
+</template>
